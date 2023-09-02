cfac6bac86c959ad3a985ce715bf6f317f1f1a37
Killian Godmez <kgodmez@gmail.com>
2023-09-02 02:20:32 +0200

    fix login admin / fix dashboard cards click / fix sort date

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,26 +131,28 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
+    if (this.counter === undefined || this.index !== index) this.counter = 0;
+    if (this.index === undefined || this.index !== index) this.index = index;
     if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
+      $(`#arrow-icon${this.index}`).css({ transform: "rotate(0deg)" });
       $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+      .html(cards(filteredBills(bills, getStatus(this.index)))
+      );
+      this.counter++;
     } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+      $(`#arrow-icon${this.index}`).css({ transform: "rotate(90deg)" });
+      $(`#status-bills-container${this.index}`).html("");
+      this.counter++;
     }
-
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
-    return bills
-
+  
+    bills.forEach((bill) => {
+      $(`#open-bill${bill.id}`)
+        .off("click")
+        .on("click",(e) => this.handleEditTicket(e, bill, bills));
+    });
+  
+  
+    return bills;
   }
 
   getBillsAllUsers = () => {
--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -19,9 +19,16 @@ const row = (bill) => {
     `)
   }
 
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+  const rows = (data) => {
+    return (data && data.length) ? data.sort((a, b) => {
+            const d1 = new Date(b.date)
+            const d2 = new Date(a.date)
+            return d1 - d2
+          })
+          .map((bill) => row({...bill, date:formatDate(bill.date)}))
+          .join('')
+      : ''
+  }
 
 export default ({ data: bills, loading, error }) => {
   
