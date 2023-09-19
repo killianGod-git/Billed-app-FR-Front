b4624295e20daf0451dc08d1e0ffbf0f4f8503f5
Killian Godmez <kgodmez@gmail.com>
2023-09-19 18:17:54 +0200

    fix git

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -130,21 +130,6 @@ export default class {
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
-<<<<<<< HEAD
-handleShowTickets(e, bills, index) {
-  if (this.counter === undefined || this.index !== index) this.counter = 0;
-  if (this.index === undefined || this.index !== index) this.index = index;
-  if (this.counter % 2 === 0) {
-    $(`#arrow-icon${this.index}`).css({ transform: "rotate(0deg)" });
-    $(`#status-bills-container${this.index}`)
-    .html(cards(filteredBills(bills, getStatus(this.index)))
-    );
-    this.counter++;
-  } else {
-    $(`#arrow-icon${this.index}`).css({ transform: "rotate(90deg)" });
-    $(`#status-bills-container${this.index}`).html("");
-    this.counter++;
-=======
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0;
     if (this.index === undefined || this.index !== index) this.index = index;
@@ -168,18 +153,7 @@ handleShowTickets(e, bills, index) {
   
   
     return bills;
->>>>>>> cfac6bac86c959ad3a985ce715bf6f317f1f1a37
   }
-
-  bills.forEach((bill) => {
-    $(`#open-bill${bill.id}`)
-      .off("click")
-      .on("click",(e) => this.handleEditTicket(e, bill, bills));
-  });
-
-
-  return bills;
-}
 
   getBillsAllUsers = () => {
     if (this.store) {
